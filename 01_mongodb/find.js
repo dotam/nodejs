a9f8f8912daa58804f084a87e46fa22be5294463
tam <tam@rasia.info>
2015-12-30 17:19:53 +0700

    remove and update mongodb

--- 01_mongodb/find.js
+++ 01_mongodb/find.js
@@ -4,7 +4,24 @@ var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
 
 var findRestaurants = function(db, callback) {
-   var cursor =db.collection('restaurants').find( );
+   // var cursor =db.collection('restaurants').find( );
+    // var cursor =db.collection('restaurants').find( { "borough": "Manhattan" } );//Query by a Top Level Field
+
+  //  var cursor =db.collection('restaurants').find( { "address.zipcode": "10075" } );//Query by a Field in an Embedded Document
+  
+   // var cursor =db.collection('restaurants').find( { "grades.grade": "B" } );//Query by a Field in an Array
+  
+   //var cursor =db.collection('restaurants').find( { "grades.score": { $gt: 30 } } );//Greater Than Operator ($gt)
+   
+   // var cursor =db.collection('restaurants').find(
+   //   { "cuisine": "Italian", "address.zipcode": "10075" }
+   // );//Logical AND
+
+	// var cursor =db.collection('restaurants').find(
+ //       { $or: [ { "cuisine": "Italian" }, { "address.zipcode": "10075" } ] }
+ //   );//Logical OR
+
+	var cursor =db.collection('restaurants').find().sort( { "borough": 1, "address.zipcode": 1 } );//Sort Query Results
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
